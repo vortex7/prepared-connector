73ec7252a5e9111b2305634d18a5c99a11eb28a9
vortex7 <sean.wright.vortex@gmail.com>
2019-10-29 18:48:23 -0700

    Modified player

--- src/components/player.js
+++ src/components/player.js
@@ -1,4 +1,5 @@
 const Scout = require("@scoutsdk/server-sdk")
+const _ = require("lodash")
 
 class Player {                  
   constructor(options) {       
@@ -25,10 +26,26 @@ class Player {
     }
 
     if(playerId) {
-      console.log(`${this.options.identifier}: ${playerId}`)
+      console.log(`Retrieving ${this.options.identifier}: ${playerId}`)
       try {
-        let player = await Scout.players.get(this.options.title, playerId, "*")
-        console.log(JSON.stringify(player, null, 2))
+        let data = await Scout.players.get(this.options.title, playerId, "*")
+        
+        let metadata = _.keyBy(data.segments[0].metadata, "key")
+
+        let stats = {}         
+        data.stats.forEach((stat) => {  
+          stats[stat.metadata.key] = {    
+            value: stat.value, 
+            displayValue: stat.displayValue 
+          }                    
+        })
+  
+        let player = {         
+          name: this.options.identifier,       
+          image: `${process.env.destinyUrl}/${metadata.emblemPath.value}`,
+          stats: stats         
+        }
+
         return player
       }
       catch(e) {
